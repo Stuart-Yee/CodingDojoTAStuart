df067bebabb750efcd07f63d5e3beebea825bf6b
Stuart-Yee <ki6fzb@gmail.com>
2022-05-28 12:44:07 -0700

    corrected formatted string variable order

--- UCard/fstrings/fstring.py
+++ UCard/fstrings/fstring.py
@@ -7,7 +7,7 @@ print("The word of the day is " + a_word + ". My favorite number is "+str(a_numb
 
 print("The word of the days is", a_word, "My favorite number is", a_number)
 
-print("The word of the day is %s. My favorite number is %d and I like to eat %s" % (a_word, a_different_word, a_number))
+print("The word of the day is %s. My favorite number is %d and I like to eat %s" % (a_word, a_number, a_different_word))
 
 the_setence = "The word of the days is", a_word, "My favorite number is", a_number
 my_data_structure = (1, False, "banana")

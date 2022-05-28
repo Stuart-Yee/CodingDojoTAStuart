184ecd5e888c8a61b5a0f809ca68bd7487f9a4d3
Stuart-Yee <ki6fzb@gmail.com>
2022-05-28 12:39:41 -0700

    demonstrated different ways to print variables and use formatted strings

---
+++ UCard/fstrings/fstring.py
@@ -0,0 +1,20 @@
+a_word = "Steps"
+a_number = 23
+a_different_word = "pineapple"
+print(f"The word of the day is {a_word}. My favorite number is {a_number}")
+
+print("The word of the day is " + a_word + ". My favorite number is "+str(a_number))
+
+print("The word of the days is", a_word, "My favorite number is", a_number)
+
+print("The word of the day is %s. My favorite number is %d and I like to eat %s" % (a_word, a_different_word, a_number))
+
+the_setence = "The word of the days is", a_word, "My favorite number is", a_number
+my_data_structure = (1, False, "banana")
+print(the_setence)
+print(my_data_structure)
+
+# is a list? no, not quite
+# is a dictionary? nope
+# IT IS A TUPLE
+#
